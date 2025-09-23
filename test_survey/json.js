295d38504fea3aa32771a4d68e1d276ff1a46f42
jonas-peschel <jonaspeschel@gmx.de>
2025-09-23 14:42:09 +0200

    test survey

---
+++ test_survey/json.js
@@ -0,0 +1,17 @@
+const json = {
+  "title": "Test survey",
+  "description": "Please help me...",
+  "pages": [
+    {
+      "name": "page1",
+      "elements": [
+        {
+          "type": "slider",
+          "name": "question1",
+          "title": "How do you rate this?"
+        }
+      ]
+    }
+  ],
+  "headerView": "advanced"
+}
